0a091286bd6a6d8d1d62749ca22c6f60f0ae559b
Colby Melvin <colby@pedago.com>
2024-10-04 10:13:34 -0400

    fix(android): ensure WX entry activity classes are properly setup in Capacitor (#6)

--- src/android/WXEntryActivity.java
+++
@@ -1,4 +0,0 @@
-package __PACKAGE_NAME__.wxapi;
-
-public class WXEntryActivity extends EntryActivity {
-}
--- src/android/WXPayEntryActivity.java
+++
@@ -1,5 +0,0 @@
-package __PACKAGE_NAME__.wxapi;
-
-public class WXPayEntryActivity extends EntryActivity {
-
-}
