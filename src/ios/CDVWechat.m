64de49269f206bf363b978c9b929481c1533d418
Colby Melvin <colby@pedago.com>
2024-10-28 10:02:31 -0400

    removing more dead code

--- src/ios/CDVWechat.m
+++ src/ios/CDVWechat.m
@@ -69,64 +69,20 @@ static int const MAX_THUMBNAIL_SIZE = 320;
     }];
 }
 
-- (void)jumpToWechat:(CDVInvokedUrlCommand *)command
-{
-    // check arguments
-    NSString *url = [command.arguments objectAtIndex:0];
-    if (!url || ![url hasPrefix:@"weixin://"])
-    {
-        [self failWithCallbackID:command.callbackId withMessage:@"参数格式错误"];
-        return ;
-    }
-
-    NSURL *formatUrl = [NSURL URLWithString:[url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
-    if ([[UIApplication sharedApplication] canOpenURL:formatUrl]) {
-        NSDictionary *options = @{UIApplicationOpenURLOptionUniversalLinksOnly : @YES};
-        [[UIApplication sharedApplication] openURL:formatUrl options:options completionHandler:nil];
-    } else{
-        [self failWithCallbackID:command.callbackId withMessage:@"未安装微信或其他错误"];
-    }
-    return ;
-}
-
 
 
 #pragma mark "WXApiDelegate"
-
--(NSString*)toJsonString: (NSDictionary *)params
-{
-    NSError  *error;
-    NSData   *data       = [NSJSONSerialization dataWithJSONObject:params options:0 error:&error];
-    NSString *jsonString = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
-    return jsonString;
-}
 
 /**
  * On wechat request
  */
 - (void)onReq:(BaseReq *)req
 {
+    // Noop
     NSLog(@"%@", req);
-    
-    // 获取开放标签传递的extinfo数据逻辑
-    if ([req isKindOfClass:[LaunchFromWXReq class]])
-    {
-        WXMediaMessage *msg = ((LaunchFromWXReq*)req).message;
-        NSString *extinfo = msg.messageExt;
-        
-        NSLog(@"extinfo = %@", extinfo);
-        if(sharedWechatPlugin) {
-            NSDictionary *params = @{@"extinfo": extinfo};
-            dispatch_async(dispatch_get_main_queue(), ^{
-              [sharedWechatPlugin.commandDelegate evalJs:[NSString stringWithFormat:@"cordova.fireDocumentEvent('wechat.%@',%@)",
-                                                         WechatDocumentEvent_LaunchFromWXReq,
-                                                         [self toJsonString:params]]];
-            });
-        }
-    }
 }
 
-- (void)onResp:(WXLaunchMiniProgramResp *)resp
+- (void)onResp:(BaseResp *)resp
 {
     BOOL success = NO;
     NSString *message = @"Unknown";
@@ -178,15 +134,6 @@ static int const MAX_THUMBNAIL_SIZE = 320;
 
             [self.commandDelegate sendPluginResult:commandResult callbackId:self.currentCallbackId];
         }
-        else if ([resp isKindOfClass:[WXLaunchMiniProgramResp class]])
-        {
-            NSString *extMsg = resp.extMsg;
-            response = @{
-                         @"extMsg": extMsg
-                         };
-            CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:response];
-            [self.commandDelegate sendPluginResult:commandResult callbackId:self.currentCallbackId];
-        }
         else
         {
             [self successWithCallbackID:self.currentCallbackId];
@@ -215,69 +162,6 @@ static int const MAX_THUMBNAIL_SIZE = 320;
 
 #pragma mark "Private methods"
 
-- (NSData *)getNSDataFromURL:(NSString *)url
-{
-    NSData *data = nil;
-
-    if ([url hasPrefix:@"http://"] || [url hasPrefix:@"https://"])
-    {
-        data = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
-    }
-    else if ([url hasPrefix:@"data:image"])
-    {
-        // a base 64 string
-        NSURL *base64URL = [NSURL URLWithString:url];
-        data = [NSData dataWithContentsOfURL:base64URL];
-    }
-    else if ([url rangeOfString:@"temp:"].length != 0)
-    {
-        url =  [NSTemporaryDirectory() stringByAppendingPathComponent:[url componentsSeparatedByString:@"temp:"][1]];
-        data = [NSData dataWithContentsOfFile:url];
-    }
-    else
-    {
-        // local file
-        url = [[NSBundle mainBundle] pathForResource:[url stringByDeletingPathExtension] ofType:[url pathExtension]];
-        data = [NSData dataWithContentsOfFile:url];
-    }
-
-    return data;
-}
-
-- (UIImage *)getUIImageFromURL:(NSString *)url
-{
-    NSData *data = [self getNSDataFromURL:url];
-    UIImage *image = [UIImage imageWithData:data];
-
-    if (image.size.width > MAX_THUMBNAIL_SIZE || image.size.height > MAX_THUMBNAIL_SIZE)
-    {
-        CGFloat width = 0;
-        CGFloat height = 0;
-
-        // calculate size
-        if (image.size.width > image.size.height)
-        {
-            width = MAX_THUMBNAIL_SIZE;
-            height = width * image.size.height / image.size.width;
-        }
-        else
-        {
-            height = MAX_THUMBNAIL_SIZE;
-            width = height * image.size.width / image.size.height;
-        }
-
-        // scale it
-        UIGraphicsBeginImageContext(CGSizeMake(width, height));
-        [image drawInRect:CGRectMake(0, 0, width, height)];
-        UIImage *scaled = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-
-        return scaled;
-    }
-
-    return image;
-}
-
 - (void)successWithCallbackID:(NSString *)callbackID
 {
     [self successWithCallbackID:callbackID withMessage:@"OK"];
@@ -298,22 +182,6 @@ static int const MAX_THUMBNAIL_SIZE = 320;
 {
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
     [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
-}
-
--  (void)openMiniProgram:(CDVInvokedUrlCommand *)command
-{
-    NSDictionary *params = [command.arguments objectAtIndex:0];
-    WXLaunchMiniProgramReq *launchMiniProgramReq = [WXLaunchMiniProgramReq object];
-    launchMiniProgramReq.userName = [params objectForKey:@"userName"];  //拉起的小程序的username
-    launchMiniProgramReq.path = [params objectForKey:@"path"];    //拉起小程序页面的可带参路径，不填默认拉起小程序首页
-    launchMiniProgramReq.miniProgramType = (int)[[params objectForKey:@"miniprogramType"] integerValue]; //拉起小程序的类型
-    [WXApi sendReq:launchMiniProgramReq completion:^(BOOL success) {
-        if(success) {
-             self.currentCallbackId = command.callbackId;
-        } else {
-            [self failWithCallbackID:command.callbackId withMessage:@"打开请求失败"];
-        }
-    }];
 }
 
 - (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
